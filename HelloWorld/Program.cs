411d13ec5877cd3e941a7adf43705c89993082d8
Steffen <Steffen@BLUECOYOTE1>
2016-08-24 21:19:34 -0500

    Add project files.

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        /// <summary>
+        /// Simple hello world application. This is ment to be more of
+        /// an introduction into using the GitHub Repository then an 
+        /// excerise in C#. 
+        /// </summary>
+        /// <param name="args">Program arguments that I'm going to ignore</param>
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+            Console.WriteLine("Press Any Key to Continue. . .");
+            Console.ReadKey();
+        }
+    }
+}
